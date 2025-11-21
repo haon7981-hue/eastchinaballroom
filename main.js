b6b0c544cd0b37e600f7b29625fe851649899e37
haon7981-hue <haon7981@gmail.com>
2025-11-21 15:08:01 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,25 @@
-// -----------------------------
-// 江浙沪砂舞 RPG - 主入口 main.js
-// -----------------------------
-
-import BootScene from "./scenes/BootScene.js";
-import PreloadScene from "./scenes/PreloadScene.js";
-import StreetScene from "./scenes/StreetScene.js";
-import BallroomScene from "./scenes/BallroomScene.js";
-import DialogUI from "./scenes/DialogUI.js";
+// main.js - 游戏入口
 
 let game = new Phaser.Game({
-    type: Phaser.AUTO,
-    width: 1280,
-    height: 720,
-    pixelArt: true,
-    backgroundColor: "#000000",
+  type: Phaser.AUTO,
+  width: 1280,
+  height: 720,
+  pixelArt: true,
+  backgroundColor: "#000",
 
-    physics: {
-        default: "arcade",
-        arcade: {
-            debug: false,
-            gravity: { y: 0 }
-        }
-    },
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false,
+      gravity: { y: 0 }
+    }
+  },
 
-    scene: [
-        BootScene,
-        PreloadScene,
-        StreetScene,
-        BallroomScene,
-        DialogUI
-    ]
+  scene: [
+    BootScene,
+    PreloadScene,
+    StreetScene,
+    BallroomScene,
+    DialogUI
+  ]
 });
-
